f44286f85672fa0b385f88baccc9a03268de4890
rembo10 <rembo10@users.noreply.github.com>
2022-11-13 17:55:03 +0530

    revert to nix-community/poetry2nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
-      url = "github:K900/poetry2nix/hack";
+      url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
